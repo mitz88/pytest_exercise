4605f16d6d866528287ace60a7b6fedaed65aaa5
Live session user <kumarmitesh88215@gmail.com>
2024-08-08 01:19:27 +0530

    first test updated

--- test_parameterize_1.py
+++ test_parameterize_1.py
@@ -1,20 +1,20 @@
 #! /bin/python3
 
-## test_parametrize_0.py
-## This is a simle test demonstrating how parametrized inputs are given to a pytest test.
+## test_parametrize_1.py
+## This is a simle test demonstrating how two parametrized inputs are given to a pytest test.
 ## Author: Mitesh Kumar
 ## Date  : 7-Aug-2024
 
 
 import pytest
 
-# A simple square function
+# A simple multiply function
 def square(num):
         return num * num
 
 
-@pytest.mark.parametrize("num", [2,4,6,8,10])
-def test_square(num):
-        result = square(num)
-        assert result == num ** 2
+@pytest.mark.parametrize("x,y", [(2,4),(4,16),(6,36),(8,64),(10,100)])
+def test_square(x,y):
+        result = square(x)
+        assert result == y
 
